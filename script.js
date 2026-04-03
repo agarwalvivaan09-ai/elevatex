859c0af74e32e412211014a12b3168bd80ea5c4d
agarwalvivaan09-ai <agarwalvivaan09@gmail.com>
2026-04-03 13:47:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -105,10 +105,4 @@ const blogObserver = new IntersectionObserver(entries => {
 
 blogCards.forEach(card => blogObserver.observe(card));
 
-const toggle = document.getElementById("menuToggle");
-const menu = document.getElementById("mobileMenu");
-
-toggle.addEventListener("click", () => {
-  menu.classList.toggle("open");
-});
 });
